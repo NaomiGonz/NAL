f2fd982faaf73afa86f13f1eafbdc43344c411ad
Anvitha Nekkanti <anvithan@bu.edu>
2025-03-01 18:58:10 -0500

    added the font theme

--- web-app/src/pages/Resource.jsx
+++ web-app/src/pages/Resource.jsx
@@ -1,11 +1,24 @@
+// import React from 'react';
+
+// const Resource = () => {
+//   return (
+//     <div className="bg-ivory"> {/* Wrapper div to cover padding area */}
+//       <h1 className="py-20">
+//         Resource Page
+//       </h1>
+//     </div>
+//   );
+// };
+
+// export default Resource;
+
+
 import React from 'react';
 
 const Resource = () => {
   return (
-    <div className="bg-ivory"> {/* Wrapper div to cover padding area */}
-      <h1 className="py-20">
-        Resource Page
-      </h1>
+    <div className="py-20 bg-ivory font-onest font-light"> {/* Apply light weight */}
+      Resource Page
     </div>
   );
 };
