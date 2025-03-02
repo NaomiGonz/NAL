4696dc790f53aa153b7619e66a9954b62af7d83e
Anvitha Nekkanti <anvithan@bu.edu>
2025-03-01 21:44:22 -0500

    made edits to about page

--- web-app/src/pages/About.jsx
+++ web-app/src/pages/About.jsx
@@ -7,8 +7,9 @@ const About = () => {
       <div className="max-w-4xl mx-auto text-left">
         <h1 className="text-6xl font-bold text-mediumblue text-center text-slate-800 mb-4">About</h1>
         <h2 className="text-3xl text-slate-600 mb-8"></h2>
-          <div className="rounded-lg border border-red-500 p-6 bg-blue-50">
-            <p className="text-slate-600 font-light text-darkblue text-lg leading-relaxed mb-8">
+          <div className="">
+            <p className="border text-darkblue font-light text-lg leading-relaxed mb-2
+            rounded-xl bg-opacity-50 bg-lightlightblue p-6">
               Millions of people struggle to find accurate diagnoses—often navigating multiple 
               specialists and tests before uncovering the right answers. Our mission is to shorten
               that journey by using a simple, quiz-based approach. With every question, we aim 
@@ -19,17 +20,17 @@ const About = () => {
             </p>
           </div>
      <div className="flex items-center p-8 mt-8">
-          <p className="text-2xl text-green-800 font-semibold transform -rotate-90 mr-6">
+          <p className="text-4xl text-green font-semibold transform -rotate-90">
             Our Team
           </p>
-          <div className="w-48 h-48 rounded-full overflow-hidden mr-6">
+          <div className="w-48 h-48 rounded-[50px] overflow-hidden mr-10 border">
             <img
               src={teamPhoto}
               alt="Our Team"
               className="object-cover w-full h-full"
             />
           </div>
-          <p className="text-slate-600 font-light text-blue-900 text-lg leading-relaxed">
+          <p className="text-darkblue font-light text-lg leading-relaxed">
             Naomi Gonzalez, Boston University CE 2025
             <br />
             Anvitha Nekkanti, Boston University BME 2025
